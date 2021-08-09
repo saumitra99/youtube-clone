e221b5e4fa38a4105a51809f18db66f4e68463fc
saumitra99 <saumitra9tiwari@gmail.com>
2021-08-10 00:07:48 +0530

    added logo of skill and education offline

--- src/Components/Explore/ExplorePage.js
+++ src/Components/Explore/ExplorePage.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ExploreCard from './ExploreCard/ExploreCard'
 import './ExplorePage.css'
-import EducationIcon from '../../media/educationLogo.png';
-import ProjectsLogo from '../../media/ProjectsLogo.png';
+import Skills from '../../media/skills.png'
+import Education from '../../media/education.png'
 // import EducationIcon from '../../media/educationLogo.png';
 // import EducationIcon from '../../media/educationLogo.png';
 function SearchPage() {
@@ -10,8 +10,8 @@ function SearchPage() {
         <div className="explorePage">
             {/* <h4>Skills</h4>  */}
             <ExploreCard icon="https://www.youtube.com/img/explore/destinations/icons/trending_color_64.png" title="Experience" />
-            <ExploreCard icon="https://img-premium.flaticon.com/png/512/3080/premium/3080467.png?token=exp=1628422544~hmac=40fef1ac9f99e2db4c4f09d34dbb7940" title="Skills" />
-            <ExploreCard icon="https://image.flaticon.com/icons/png/512/2683/2683272.png" title="Projects" />
+            <ExploreCard icon={Skills} title="Skills" />
+            <ExploreCard icon={Education} title="Projects" />
             <ExploreCard icon="https://img-premium.flaticon.com/png/512/2936/premium/2936735.png?token=exp=1628422589~hmac=ab69ed88e049e85a2b755ccaa222e73c" title="Education" />
             <ExploreCard icon="https://image.flaticon.com/icons/png/512/1973/1973739.png" title="Language" />
         </div>
